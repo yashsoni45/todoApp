df296e3c680cc743e89f35aec1f39a1fdae762b5
yashsoni45 <yashraj4549@gmail.com>
2024-08-15 19:18:07 +0530

    todoApp using express and mongoDB

---
+++ controllers/createTodo.js
@@ -0,0 +1,37 @@
+const Todo= require("../models/Todo");
+
+//define route handler
+
+exports.createTodo= async(req,res) => {
+    try{
+          // extract title and description from request body
+          const {title,description}= req.body;
+
+          //create new Todo Obj and insert in DB
+          const response = await Todo.create({title,description});
+
+          //send a json response with a success flag
+          res.status(200).json(
+            {
+                success:true,
+                data:response,
+                message:"Enter Created Successfully"
+            }
+          );
+
+    }
+
+    catch(err){
+            console.error(err);
+            console.log(err);
+            res.status(500)
+            .json(
+                {
+                success:false,
+                data:"Internal server error",
+                message:err.message
+            }
+        
+        );
+    }
+}
